12a5d8466ea5968602f8c79b4ad9c3c07c5aaf73
Amina Mazouzi <amina.mazouzi@etud.u-picardie.fr>
2025-10-28 17:04:21 +0100

    fix: Ré-indexation de tous les fichiers après merge

--- UniversiteDomain/Entities/Etudiant.cs
+++ UniversiteDomain/Entities/Etudiant.cs
@@ -2,18 +2,17 @@
 using UniversiteDomain.Util;
 
 namespace UniversiteDomain.Entities;
-public class Etudiant
+public class Parcours
 {
     public long Id { get; set; }
-    public string NumEtud { get; set; } = string.Empty;
-    public string Nom { get; set; } = string.Empty;
-    public string Prenom { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
+    public string NomParcours { get; set; } = String.Empty;
+    public int AnneeFormation { get; set; } = 1;
+    // OneToMany : un parcours contient plusieurs étudiants
+    // Remarque : pour éviter quelques NullPointerException disgracieux, j'ai choisi de créer une liste d'incrits vide quand aucun étudiant n'est inscrit dans un parcours plutôt que de l'initialiser à null
+    public List<Etudiant>? Inscrits { get; set; } = new();
 
     public override string ToString()
     {
-        return $"ID {Id} : {NumEtud} - {Nom} {Prenom} inscrit en "/*+ParcoursSuivi*/;
+        return "ID "+Id +" : "+NomParcours+" - Master "+AnneeFormation;
     }
-    
-   
 }
--- UniversiteDomainUnitTest/ParcoursUnitTests.cs
+++ UniversiteDomainUnitTest/ParcoursUnitTests.cs
@@ -15,6 +15,7 @@ public class ParcoursUnitTests
     }
 
     [Test]
+
     public async Task CreateParcoursUseCase()
     {
         // Arrange - Préparation des données de test
@@ -172,4 +173,48 @@ public class ParcoursUnitTests
         // Vérification du message d'erreur
         Assert.That(exception!.Message, Does.Contain("année").IgnoreCase);  // ⬅️ Correction syntaxe
     }
+   
+    [Test]
+    public async Task AddEtudiantDansParcoursUseCase()
+    {
+        long idEtudiant = 1;
+        long idParcours = 3;
+        Etudiant etudiant= new Etudiant { Id = 1, NumEtud = "1", Nom = "nom1", Prenom = "prenom1", Email = "1" };
+        Parcours parcours = new Parcours{Id=3, NomParcours = "Ue 3", AnneeFormation = 1};
+        
+        // On initialise une fausse datasource qui va simuler un EtudiantRepository
+        var mockEtudiant = new Mock<IEtudiantRepository>();
+        var mockParcours = new Mock<IParcoursRepository>();
+        List<Etudiant> etudiants = new List<Etudiant>();
+        etudiants.Add(new Etudiant{Id=1});
+        mockEtudiant
+            .Setup(repo=>repo.FindByConditionAsync(e=>e.Id.Equals(idEtudiant)))
+            .ReturnsAsync(etudiants);
+
+        List<Parcours> parcourses = new List<Parcours>();
+        parcourses.Add(parcours);
+        
+        List<Parcours> parcoursFinaux = new List<Parcours>();
+        Parcours parcoursFinal = new Parcours{Id=3, NomParcours = "Ue 3", AnneeFormation = 1};
+        parcoursFinal.Inscrits.Add(etudiant);
+        parcoursFinaux.Add(parcours);
+        
+        mockParcours
+            .Setup(repo=>repo.FindByConditionAsync(e=>e.Id.Equals(idParcours)))
+            .ReturnsAsync(parcourses);
+        mockParcours
+            .Setup(repo => repo.AddEtudiantAsync(idParcours, idEtudiant))
+            .ReturnsAsync(parcoursFinal);
+        // Création du use case en utilisant le mock comme datasource
+        AddEtudiantDansParcoursUseCase useCase=new AddEtudiantDansParcoursUseCase(mockEtudiant.Object, mockParcours.Object);
+        
+        // Appel du use case
+        var parcoursTest=await useCase.ExecuteAsync(idParcours, idEtudiant);
+        // Vérification du résultat
+        Assert.That(parcoursTest.Id, Is.EqualTo(parcoursFinal.Id));
+        Assert.That(parcoursTest.Inscrits, Is.Not.Null);
+        Assert.That(parcoursTest.Inscrits.Count, Is.EqualTo(1));
+        Assert.That(parcoursTest.Inscrits[0].Id, Is.EqualTo(idEtudiant));
+    }
+}
 }
